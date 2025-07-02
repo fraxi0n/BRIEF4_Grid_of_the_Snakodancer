8494c3879f9d5d48a5ffee6a741f4897ce49f35c
fraxi0n <85115114+fraxi0n@users.noreply.github.com>
2025-07-02 09:29:16 +0200

    add base loop

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,49 @@
 const test: string = "Hello World";
 
 let number : number
+
+let keyUp = false
+let keyDown = false
+let keyRight = false
+let keyLeft = false
+
+
+function KeyUp (t: { preventDefault: () => void; code: string; })
+{
+  t.preventDefault();
+  if (t.code == "ArrowUp") { keyUp= false}
+  if (t.code == "ArrowDown") { keyDown= false}
+  if (t.code == "ArrowLeft") { keyLeft= false}
+  if (t.code == "ArrowRight") { keyRight= false}
+
+}
+
+let dt : number
+let time = Date.now();
+
+let timeGame = 0 
+
+const  update  = (dt: number) =>  {
+  timeGame+=dt
+
+
+
+
+  console.log(dt, timeGame)
+}
+
+const  draw = () =>{
+}
+
+function loop() {
+  const now = Date.now();
+   dt = now - time;
+
+  update(dt);
+  draw();
+
+  time = now;
+  requestAnimationFrame(loop); 
+}
+
+requestAnimationFrame(loop); 
